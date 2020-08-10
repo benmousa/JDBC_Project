84406c31a4ea261d16cd5aae0ec671793fa42839
Mohammed Ali <moh_benmousa@yahoo.com>
2020-08-10 17:17:02 -0400

    Iterating using WHILE LOOP

---
+++ src/test/java/com/cybertek/JDBC/Day01/Country_Table.java
@@ -0,0 +1,23 @@
+package com.cybertek.JDBC.Day01;
+
+import java.sql.*;
+
+public class Country_Table {
+    public static void main(String[] args) throws SQLException {
+
+
+        String connectionStr = "jdbc:oracle:thin:@100.25.148.1:1521:xe";
+        String userName = "hr";
+        String passWord = "hr";
+        Connection con = DriverManager.getConnection (connectionStr, userName, passWord);
+        Statement statement = con.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM COUNTRIES");
+
+        while (resultSet.next() == true){
+            System.out.println(resultSet.getString("COUNTRY_ID")+" "+
+                               resultSet.getString("COUNTRY_NAME")+" "+
+                               resultSet.getString("REGION_ID"));
+        }
+        System.out.println("THE END ");
+    }
+}
