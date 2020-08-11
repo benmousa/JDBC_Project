ef96cc88d075f017afe0bc283f08d3e9e9997f1b
Mohammed Ali <moh_benmousa@yahoo.com>
2020-08-10 20:44:19 -0400

    Iterating backward

--- src/test/java/com/cybertek/JDBC/Day01/MovingForward_Backward_WithResult.java
+++ src/test/java/com/cybertek/JDBC/Day01/MovingForward_Backward_WithResult.java
@@ -13,6 +13,33 @@ public class MovingForward_Backward_WithResult {
         Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet resultSet = statement.executeQuery("SELECT * FROM COUNTRIES");
 
+        resultSet.next();
+        System.out.println(resultSet.getString("COUNTRY_ID")+" "+
+                resultSet.getString("COUNTRY_NAME"));
+
+        resultSet.next();
+        System.out.println(resultSet.getString("COUNTRY_ID")+" "+
+                resultSet.getString("COUNTRY_NAME"));
+
+        resultSet.previous();
+        System.out.println(resultSet.getString("COUNTRY_ID")+" "+
+                resultSet.getString("COUNTRY_NAME"));
+
+        resultSet.first();
+        System.out.println(resultSet.getString("COUNTRY_ID")+" "+
+                resultSet.getString("COUNTRY_NAME"));
+
+        resultSet.last();
+        System.out.println(resultSet.getString("COUNTRY_ID")+" "+
+                resultSet.getString("COUNTRY_NAME"));
+
+        resultSet.absolute(5);
+        System.out.println(resultSet.getString("COUNTRY_ID")+" "+
+                resultSet.getString("COUNTRY_NAME"));
+
+        resultSet.close();
+        statement.close();
+        con.close();
         System.out.println("THE END ");
     }
 }
